086ec0ddd7bebc0030cd0026f9203fd166dc1452
DennyLuong <denny.luong@outlook.com>
2018-03-26 16:18:58 -0500

    Working Code
    Commented Lines Might Work

--- app/src/main/java/com/example/denny/mapstest/MainActivity.java
+++ app/src/main/java/com/example/denny/mapstest/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.denny.mapstest;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -15,10 +19,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    //TODO Change Device Address and UUID
+    private final String DEVICE_ADDRESS="00:06:66:D2:A8:E7"; //Bluesmirf BTA BlueTooth Address
+    //UUID is General Usage. *I think*
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
+
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+
+    Button startButton, sendButton, stopButton, clearButton;
+    TextView textView;
+    EditText editText;
+    Button mapViewer;
+
+    boolean deviceConnected=false;
+    Thread thread;
+    byte buffer[];
+    int bufferPosition;
+    boolean stopThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -29,29 +62,174 @@ public class MainActivity extends AppCompatActivity {
         final int duration = Toast.LENGTH_SHORT;
 
         final Toast toast = Toast.makeText(context, text, duration);
-
-        Button gMaps = (Button) findViewById(R.id.googsMaps);
-        Button mapViewer = (Button) findViewById(R.id.mapview);
-
-        gMaps.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                toast.show();
-                Toast.makeText(context, text, duration).show();
-                Uri gmmIntentUri = Uri.parse("geo:37.7749, -122.4194");
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
-            }
-        });
+//        startButton = (Button) findViewById(R.id.connectBT);
+        mapViewer = (Button) findViewById(R.id.mapview);
+        textView = (TextView) findViewById(R.id.value);
+//        stopButton = (Button) findViewById(R.id.buttonStop);
+//        clearButton = (Button) findViewById(R.id.buttonClear);
+//        sendButton = (Button) findViewById(R.id.buttonSend);
+//        setUiEnabled(false);
 
         mapViewer.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                toast.show();
-//                Toast.makeText(context, "Opening Map Viewer Screen", duration).show();
+//                toast.show();
+                Toast.makeText(context, "Opening Map Viewer Screen", duration).show();
                 openMapViewerActivity();
             }
         });
     }
+
+//    public void setUiEnabled(boolean bool)
+//    {
+//        startButton.setEnabled(!bool);
+//        sendButton.setEnabled(bool);
+//        stopButton.setEnabled(bool);
+//        textView.setEnabled(bool);
+//    }
+
+//    public boolean BTinit()
+//    {
+//        boolean found=false;
+//        BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+//        if (bluetoothAdapter == null) {
+//            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
+//        }
+//        if(!bluetoothAdapter.isEnabled())
+//        {
+//            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(enableAdapter, 0);
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+//        if(bondedDevices.isEmpty())
+//        {
+//            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+//        }
+//        else
+//        {
+//            for (BluetoothDevice iterator : bondedDevices)
+//            {
+//                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+//                {
+//                    device=iterator;
+//                    found=true;
+//                    break;
+//                }
+//            }
+//        }
+//        return found;
+//    }
+
+//    public boolean BTconnect()
+//    {
+//        boolean connected=true;
+//        try {
+//            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+//            socket.connect();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            connected=false;
+//        }
+//        if(connected)
+//        {
+//            try {
+//                outputStream=socket.getOutputStream();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                inputStream=socket.getInputStream();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//
+//        return connected;
+//    }
+
+//    public void onClickStart(View view) {
+//        if(BTinit())
+//        {
+//            if(BTconnect())
+//            {
+//                setUiEnabled(true);
+//                deviceConnected=true;
+//                beginListenForData();
+//                textView.append("\nConnection Opened!\n");
+//            }
+//
+//        }
+//    }
+
+//    void beginListenForData()
+//    {
+//        final Handler handler = new Handler();
+//        stopThread = false;
+//        buffer = new byte[1024];
+//        Thread thread  = new Thread(new Runnable()
+//        {
+//            public void run()
+//            {
+//                while(!Thread.currentThread().isInterrupted() && !stopThread)
+//                {
+//                    try
+//                    {
+//                        int byteCount = inputStream.available();
+//                        if(byteCount > 0)
+//                        {
+//                            byte[] rawBytes = new byte[byteCount];
+//                            inputStream.read(rawBytes);
+//                            final String string=new String(rawBytes,"UTF-8");
+//                            handler.post(new Runnable() {
+//                                public void run()
+//                                {
+//                                    textView.append(string);
+//                                }
+//                            });
+//
+//                        }
+//                    }
+//                    catch (IOException ex)
+//                    {
+//                        stopThread = true;
+//                    }
+//                }
+//            }
+//        });
+//
+//        thread.start();
+//    }
+
+//    public void onClickSend(View view) {
+//        String string = editText.getText().toString();
+//        string.concat("\n");
+//        try {
+//            outputStream.write(string.getBytes());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        textView.append("\nSent Data:"+string+"\n");
+//    }
+
+
+//    public void onClickStop(View view) throws IOException {
+//        stopThread = true;
+//        outputStream.close();
+//        inputStream.close();
+//        socket.close();
+//        setUiEnabled(false);
+//        deviceConnected=false;
+//        textView.append("\nConnection Closed!\n");
+//    }
+//
+//    public void onClickClear(View view) {
+//        textView.setText("");
+//    }
 
     private void openMapViewerActivity() {
         Intent intent = new Intent(this, MapsActivity.class);
--- app/src/main/java/com/example/denny/mapstest/MapsActivity.java
+++ app/src/main/java/com/example/denny/mapstest/MapsActivity.java
@@ -6,12 +6,12 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -37,9 +37,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     String phoneNo = "8326606067";
     String message = "Test Message";
 
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,14 +51,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
 
+        requestSmsPermission();
+
         //fab
         iamthefab = (FloatingActionButton) findViewById(R.id.iamfab);
         iamthefab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
                 Toast.makeText(context, "i am fab", Toast.LENGTH_LONG).show();
-//                updateUserLocation(mMap);
-                sendSMSMessage();
+                updateUserLocation(mMap);
+//                sendSMSMessage();
             };
         });
 
@@ -70,6 +69,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -85,34 +85,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
     }
 
+    public void updateUserLocation(GoogleMap mMap) {
+        LatLng mvmtTracker = new LatLng(29.72186, -95.34011);
+        mMap.addMarker(new MarkerOptions().position(mvmtTracker).title("TimeStamp: HH:MM"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mvmtTracker));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
+    }
+
     //TODO have options for updating the shoe location
     //and have another option to send a text message to check where they are.
 
+    private void requestSmsPermission() {
+        String permission = Manifest.permission.READ_SMS;
+        int grant = ContextCompat.checkSelfPermission(this, permission);
+        if (grant != PackageManager.PERMISSION_GRANTED) {
+            String[] permission_list = new String[1];
+            permission_list[0] = permission;
+            ActivityCompat.requestPermissions(this, permission_list, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Toast.makeText(context,"permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+//                Toast.makeText(context,"permission not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     protected void sendSMSMessage() {
         SmsManager smsManager = SmsManager.getDefault();
-//        StringBuilder coordinatesMsg = new StringBuilder(30).append(longitude).append(" Long | Lat ").append(latitude);
         smsManager.sendTextMessage(phoneNo, null, message, null, null);
     }
-
-    //SMS should automatically send when outside of the radius or not in the correct route.
-
-
-    private void updateUserLocation(GoogleMap googleMap) {
-        mMap = googleMap;
-//        LatLng school = new LatLng(29.719949, -95.342233);
-//        mMap.addMarker(new MarkerOptions().position(school).title("University of Houston"));
-        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-            }
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-            public void onProviderEnabled(String provider) {}
-
-            public void onProviderDisabled(String provider) {}
-
-        };
-
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-    }
 }
+
+
+//TODO
+/*
+Accept Bluetooth
+
+Send SMS when out of range.
+Update location every 20 seconds - Pintracker.
+Send SMS with location info.
+
+Connect to second phone to show on both screens
+
+*/
